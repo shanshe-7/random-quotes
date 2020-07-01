dbe5232d1519e92aa1c6d46cf8aad9a82e1160d9
shanshe kenkadze <shanshe1993@gmail.com>
2020-07-01 10:30:59 +0400

    make it better

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Quotes from './Quote'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+\
+      <Quotes />
+    </div>
+  );
+}
+
+export default App;
